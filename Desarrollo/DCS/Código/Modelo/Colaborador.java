1446f0f517d14f41af17565ffda1ec1f41723b86
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-01-17 11:05:44 -0500

    Modelo Colaborador

---
+++ Desarrollo/DCS/Código/Modelo/Colaborador.java
@@ -0,0 +1,54 @@
+package Modelo;
+
+public class Colaborador extends EntidadBase{
+    
+    private String Cargo;
+    private String Turno;
+
+    /***
+     * 
+     * @param ID
+     * @param DNI
+     * @param nombre
+     * @param apellidoPaterno
+     * @param apellidoMaterno
+     * @param sexo 
+     */
+    public Colaborador(int ID, int DNI, String nombre, String apellidoPaterno, String apellidoMaterno, String sexo) {
+        super(ID, DNI, nombre, apellidoPaterno, apellidoMaterno, sexo);
+        Cargo = "";
+        Turno = "";
+    }
+
+    /***
+     * 
+     * @return 
+     */
+    public String getCargo() {
+        return Cargo;
+    }
+
+    /***
+     * 
+     * @param Cargo 
+     */
+    public void setCargo(String Cargo) {
+        this.Cargo = Cargo;
+    }
+
+    /***
+     * 
+     * @return 
+     */
+    public String getTurno() {
+        return Turno;
+    }
+
+    /***
+     * 
+     * @param Turno 
+     */
+    public void setTurno(String Turno) {
+        this.Turno = Turno;
+    }    
+}
