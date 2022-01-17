6f41899c76e8c8a6d3f85c172b4ae16d5d8aa489
SebCéspedes <sebastian.cespedes1@unmsm.edu.pe>
2022-01-17 11:45:21 -0500

    Cargojavafile

---
+++ Desarrollo/DCS/Código/Modelo/Enum/Cargo.java
@@ -0,0 +1,34 @@
+package Modelo.Enum;
+
+/**
+ *
+ * @author Diego D.
+ * @version 1.1
+ * @since 2021
+ */
+public enum Cargo {
+    GERENTE_GENERAL("Gerente general"),
+    GERENTE_DEPARTAMENTO("Gerente de departamento"),
+    DIRECTOR_DISTRITO("Director de distrito"),
+    ATENCION_CLIENTE("Atención al cliente"),
+    ASESOR_VENTA("Asesor de venta");
+
+    private final String descripcion;
+
+    /**
+     *
+     * @param descripcion
+     */
+    private Cargo(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return descripcion;
+    }
+}
